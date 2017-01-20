e2de8206882c0164b3d300f706a236ceece929bc
Michael Dimenshtein <michaeld@michaeldi-mac.local>
2017-01-20 14:21:44 +0200

    finished tests

--- test/agent_sdk_test.js
+++ test/agent_sdk_test.js
@@ -39,7 +39,7 @@ describe('Agent SDK Tests', function () {
         mockery.disable();
     });
 
-    it('happy flow of initialization should work', function (done) {
+    it('should create an instance and publish connected event', function (done) {
         externalServices.getDomains.yieldsAsync(null, {agentVep: 'some-domain', asyncMessaging: 'another-domain'});
         externalServices.login.yieldsAsync(null, {bearer: 'im encrypted', config: {userId: 'imauser'}});
         const agent = new Agent({
@@ -53,6 +53,154 @@ describe('Agent SDK Tests', function () {
             expect(agent.connected).to.be.true;
             done();
         });
+    });
+
+    it('should fail to create an instance when csds is not available', function (done) {
+        externalServices.getDomains.yieldsAsync(new Error('cannot connect to csds'));
+        const agent = new Agent({
+            accountId: 'account',
+            username: 'me',
+            password: 'password'
+        });
+        agent.on('error', (err) => {
+            expect(err).to.be.instanceof(Error);
+            expect(err.message).to.contain('csds');
+            done();
+        });
+    });
+
+    it('should fail to create an instance when login service is not available', function (done) {
+        externalServices.getDomains.yieldsAsync(null, {agentVep: 'some-domain', asyncMessaging: 'another-domain'});
+        externalServices.login.yieldsAsync(new Error('cannot login'));
+        const agent = new Agent({
+            accountId: 'account',
+            username: 'me',
+            password: 'password'
+        });
+        agent.on('error', (err) => {
+            expect(err).to.be.instanceof(Error);
+            expect(err.message).to.contain('login');
+            done();
+        });
+    });
+
+    it('should receive all notifications', function (done) {
+        externalServices.getDomains.yieldsAsync(null, {agentVep: 'some-domain', asyncMessaging: 'another-domain'});
+        externalServices.login.yieldsAsync(null, {bearer: 'im encrypted', config: {userId: 'imauser'}});
+        const agent = new Agent({
+            accountId: 'account',
+            username: 'me',
+            password: 'password'
+        });
+        agent.on('connected', (msg) => {
+            agent.transport.emit('message', {kind: 'notification', type: 'myType', body: {x: 'x'}});
+        });
+
+        agent.on('notification', (msg) => {
+            expect(msg).to.be.defined;
+            expect(msg.body.x).to.equal('x');
+            done();
+        });
+    });
+
+    it('should receive specific notifications', function (done) {
+        externalServices.getDomains.yieldsAsync(null, {agentVep: 'some-domain', asyncMessaging: 'another-domain'});
+        externalServices.login.yieldsAsync(null, {bearer: 'im encrypted', config: {userId: 'imauser'}});
+        const agent = new Agent({
+            accountId: 'account',
+            username: 'me',
+            password: 'password'
+        });
+        agent.on('connected', (msg) => {
+            agent.transport.emit('message', {kind: 'notification', type: 'myType', body: {x: 'x'}});
+        });
+
+        agent.on('myType', (body) => {
+            expect(body).to.be.defined;
+            expect(body.x).to.equal('x');
+            done();
+        });
+    });
+
+    it('should call the request callback on response', function (done) {
+        externalServices.getDomains.yieldsAsync(null, {agentVep: 'some-domain', asyncMessaging: 'another-domain'});
+        externalServices.login.yieldsAsync(null, {bearer: 'im encrypted', config: {userId: 'imauser'}});
+        const agent = new Agent({
+            accountId: 'account',
+            username: 'me',
+            password: 'password'
+        });
+
+        agent.on('connected', (msg) => {
+            agent.getClock({some: 'data'}, (err, response) => {
+                expect(err).to.be.null;
+                expect(response).to.be.defined;
+                expect(response.x).to.equal('x');
+                done();
+            });
+
+            setTimeout(() => {
+                agent.transport.emit('message', {
+                    kind: 'resp',
+                    reqId: tranportSendStub.getCall(0).args[0].id,
+                    type: 'myRespType',
+                    body: {x: 'x'}
+                });
+            }, 10);
+        });
+
+    });
+
+    it('should emit specific response event', function (done) {
+        externalServices.getDomains.yieldsAsync(null, {agentVep: 'some-domain', asyncMessaging: 'another-domain'});
+        externalServices.login.yieldsAsync(null, {bearer: 'im encrypted', config: {userId: 'imauser'}});
+        const agent = new Agent({
+            accountId: 'account',
+            username: 'me',
+            password: 'password'
+        });
+
+        agent.on('connected', (msg) => {
+            agent.getClock({some: 'data'}, (response) => {
+            });
+
+            setTimeout(() => {
+                agent.transport.emit('message', {
+                    kind: 'resp',
+                    reqId: tranportSendStub.getCall(1).args[0].id,
+                    type: 'myRespType',
+                    body: {x: 'x'}
+                });
+            }, 10);
+        });
+
+        agent.on('myRespType', (body) => {
+            expect(body).to.be.defined;
+            expect(body.x).to.equal('x');
+            done();
+        });
+    });
+
+    it('should call the request callback with error on timeout', function (done) {
+        externalServices.getDomains.yieldsAsync(null, {agentVep: 'some-domain', asyncMessaging: 'another-domain'});
+        externalServices.login.yieldsAsync(null, {bearer: 'im encrypted', config: {userId: 'imauser'}});
+        const agent = new Agent({
+            accountId: 'account',
+            username: 'me',
+            password: 'password',
+            requestTimeout: 10,
+            errorCheckInterval: 10,
+        });
+
+        agent.on('connected', (msg) => {
+            agent.getClock({some: 'data'}, (err, response) => {
+                expect(err).to.be.defined;
+                expect(err).to.be.instanceof(Error);
+                expect(err.message).to.contain('timed');
+                done();
+            });
+        });
+
     });
 
 });
