d2b5b0bf3dae841d5a92e116c1867f24b16309eb
michaeld <michaeld@liveperson.com>
2017-03-11 17:39:40 +0200

    - small alignments

--- examples/cluster/task-sharding.js
+++ examples/cluster/task-sharding.js
@@ -8,50 +8,54 @@ class TaskSharding extends Events {
     constructor(zkConnectionString, tasks, serviceName) {
         super();
         this.serviceName = serviceName || 'my/service/name/v1';
-        var zoologistClient = Zoologist.newClient(zkConnectionString);
+        const zoologistClient = Zoologist.newClient(zkConnectionString);
         zoologistClient.start();
         this.serviceInstance = ServiceInstanceBuilder.builder().name(this.serviceName).build();
         this.serviceDiscovery = ServiceDiscoveryBuilder.builder()
-                .client(zoologistClient).basePath('services')
-                .thisInstance(this.serviceInstance).build();
-        this._onClusterChange((si, hr) => this.emit('clusterChange', si, hr));
+            .client(zoologistClient).basePath('services')
+            .thisInstance(this.serviceInstance).build();
+        this._clusterChange((si, hr) => this.emit('clusterChange', si, hr));
         this.allTasks = tasks || [];
         this.handledTasks = {};
 
         this.on('clusterChange', (myServiceInstance, updatedHashRing) => {
-            const isOwnedByMe = task => updatedHashRing.get(task.id) === myServiceInstance.data.id;
-            const myTasks = this.allTasks.filter(isOwnedByMe).reduce((acc, task) => {
-                acc[task.id] = task;
-                return acc;
-            }, {});
-
-            Object.keys(this.handledTasks)
-                    .filter(taskId => !myTasks[taskId])
-                    .forEach(oldTaskId => {
-                        this.emit('taskRemoved',this.handledTasks[oldTaskId]);
-                        delete this.handledTasks[oldTaskId];
-                    });
-
-            Object.keys(myTasks)
-                    .filter(taskId => !this.handledTasks[taskId])
-                    .forEach(newTaskId => {
-                        this.emit('taskAdded',myTasks[newTaskId],(taskInfo)=>this.handledTasks[newTaskId]=taskInfo);
-                    });
+            this._onClusterChange(myServiceInstance, updatedHashRing);
         });
     }
 
-    _onClusterChange(cb) {
-        const that = this;
-        that.serviceDiscovery.registerService((err, data) => {
-            get();
-            function get() {
-                const absPath = [that.serviceDiscovery.basePath, that.serviceName].join('/');
-                that.serviceDiscovery.client.getClientwithConnectionCheck().getChildren(absPath, function() {
-                    get();
-                }, function(err, serviceList, stat) {
-                    cb.call(this, that.serviceInstance, new HashRing(serviceList));
-                });
-            }
+    _onClusterChange(myServiceInstance, updatedHashRing) {
+        const isOwnedByMe = task => updatedHashRing.get(task.id) === myServiceInstance.data.id;
+        const myTasks = this.allTasks.filter(isOwnedByMe).reduce((acc, task) => {
+            acc[task.id] = task;
+            return acc;
+        }, {});
+
+        Object.keys(this.handledTasks)
+            .filter(taskId => !myTasks[taskId])
+            .forEach(oldTaskId => {
+                this.emit('taskRemoved', this.handledTasks[oldTaskId]);
+                delete this.handledTasks[oldTaskId];
+            });
+
+        Object.keys(myTasks)
+            .filter(taskId => !this.handledTasks[taskId])
+            .forEach(newTaskId => {
+                this.emit('taskAdded', myTasks[newTaskId], (taskInfo) => this.handledTasks[newTaskId] = taskInfo);
+            });
+    }
+
+    _clusterChange(callback) {
+        this.serviceDiscovery.registerService(() => {
+            this._watcher(callback)
+        });
+    }
+
+    _watcher(callback) {
+        const absPath = [this.serviceDiscovery.basePath, this.serviceName].join('/');
+        this.serviceDiscovery.client.getClientwithConnectionCheck().getChildren(absPath, () => {
+            this._watcher(callback);
+        }, (err, serviceList) => {
+            callback(this.serviceInstance, new HashRing(serviceList));
         });
     }
 
