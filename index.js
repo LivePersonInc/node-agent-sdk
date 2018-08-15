4670dca565f2a386e72752c755589662e5877316
ChristianJGarrido <cgarrido@liveperson.com>
2018-08-15 11:45:22 -0400

    exposing Transformer

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Agent = require('./lib/AgentSDK');
-const Transformer = require('./Transformer');
+const Transformer = require('./lib/Transformer');
 
 module.exports = {
     Agent,
