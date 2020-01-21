4674e0c20b28d1019774959d95be9a8c6a9f9d6b
Scott Wilde <swilde@liveperson.com>
2020-01-21 15:16:38 -0800

    greeting bot refactored

--- examples/greeting-bot/greeting-bot.js
+++ examples/greeting-bot/greeting-bot.js
@@ -1,74 +1,97 @@
 'use strict';
 
 const Agent = require('./../../lib/AgentSDK');
-const conf = {
-    accountId: process.env.LP_ACCOUNT,
-    username: process.env.LP_USER,
-    password: process.env.LP_PASS
-};
-if (process.env.LP_CSDS) {
-    conf.csdsDomain = process.env.LP_CSDS;
-}
-const agent = new Agent(conf);
 
-let openConvs = {};
+class GreetingBot extends Agent {
 
-agent.on('connected', () => {
-    console.log('connected...');
-    agent.setAgentState({ availability: 'AWAY' }); // Do not route me conversations, I'll join by myself.
-    agent.subscribeExConversations({
-        'convState': ['OPEN'] // subscribes to all open conversation in the account.
-    });
-    agent._pingClock = setInterval(agent.getClock, 30000);
-});
+    constructor(conf) {
+        super(conf);
 
-agent.on('cqm.ExConversationChangeNotification', notificationBody => {
-    notificationBody.changes.forEach(change => {
-        if (change.type === 'UPSERT') {
-            if (!openConvs[change.result.convId]) {
-                openConvs[change.result.convId] = change.result;
-                if (!getParticipantInfo(change.result.conversationDetails, agent.agentId)) {
-                    agent.updateConversationField({
-                        'conversationId': change.result.convId,
-                        'conversationField': [{
-                            'field': 'ParticipantsChange',
-                            'type': 'ADD',
-                            'role': 'MANAGER'
-                        }]
-                    }, () => {
-                        agent.publishEvent({
-                            dialogId: change.result.convId,
-                            event: {
-                                type: 'ContentEvent',
-                                contentType: 'text/plain',
-                                message: 'welcome from bot'
-                            }
-                        });
-                    });
+        this.conf = conf;
+
+        this.openConvs = {};
+
+        this.on('connected', this.onConnected.bind(this));
+
+        // Notification on changes in the open consversation list
+        this.on('cqm.ExConversationChangeNotification', this.onConversationNotification.bind(this));
+
+        this.on('error', err => console.log('got an error', err));
+
+        this.on('closed', data => {
+            // For production environments ensure that you implement reconnect logic according to
+            // liveperson's retry policy guidelines: https://developers.liveperson.com/guides-retry-policy.html
+            console.log('socket closed', data);
+            clearInterval(this._pingClock);
+
+            this.reconnect(); //regenerate token for reasons of authorization (data === 4401 || data === 4407)
+        });
+
+    }
+
+    onConnected() {
+        console.log('connected...');
+        this.setAgentState({ availability: 'AWAY' }); // Do not route me conversations, I'll join by myself.
+        this.subscribeExConversations({
+            'convState': ['OPEN'] // subscribes to all open conversation in the account.
+        });
+        this._pingClock = setInterval(this.getClock, 30000);
+    }
+
+    onConversationNotification(notificationBody) {
+        notificationBody.changes.forEach(change => {
+            if (change.type === 'UPSERT') {
+
+                // ignore if we've already joined
+                if (this.openConvs[change.result.convId]) return;
+
+                // add the conversation to the list
+                this.openConvs[change.result.convId] = change.result;
+
+                // determine if we've already joined the conversation
+                let isParticipant = this.getParticipantInfo(change.result.conversationDetails, this.agentId);
+
+                // if not, attempt a join
+                if (!isParticipant) {
+                    this.joinConversation(change.result);
                 }
             }
-        }
-        else if (change.type === 'DELETE') {
-            delete openConvs[change.result.convId];
-            console.log('conversation was closed.\n');
-        }
-    });
-});
+            else if (change.type === 'DELETE') {
+                delete this.openConvs[change.result.convId];
+                console.log('conversation was closed.\n');
+            }
+        });
+    }
 
-agent.on('error', err => {
-    console.log('got an error', err);
-});
+    joinConversation(conversation) {
+        this.updateConversationField({
+            'conversationId': conversation.convId,
+            'conversationField': [{
+                'field': 'ParticipantsChange',
+                'type': 'ADD',
+                'role': 'MANAGER'
+            }]
+        }, () => {
+            this.onConversationJoin(conversation)
+        });
+    }
 
+    onConversationJoin(conversation) {
+        // send a greeting
+        this.publishEvent({
+            dialogId: conversation.convId,
+            event: {
+                type: 'ContentEvent',
+                contentType: 'text/plain',
+                message: 'welcome from bot'
+            }
+        });
+    }
 
+    getParticipantInfo(convDetails, participantId) {
+        return convDetails.participants.filter(p => p.id === participantId)[0];
+    }
 
-agent.on('closed', data => {
-    // For production environments ensure that you implement reconnect logic according to
-    // liveperson's retry policy guidelines: https://developers.liveperson.com/guides-retry-policy.html
-    console.log('socket closed', data);
-    clearInterval(agent._pingClock);
-    agent.reconnect(); //regenerate token for reasons of authorization (data === 4401 || data === 4407)
-});
-
-function getParticipantInfo(convDetails, participantId) {
-    return convDetails.participants.filter(p => p.id === participantId)[0];
 }
+
+module.exports = GreetingBot;
